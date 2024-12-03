ec48603c45de69fda458ad16e5c349716249c183
Kaustav Das <kaustavdas0228@gmail.com>
2024-12-03 13:53:04 +0530

    Port env chnage

--- server/index.js
+++ server/index.js
@@ -185,7 +185,7 @@ io.on('connection', (socket) => {
 dbConnect();
 
 // Start the server
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
