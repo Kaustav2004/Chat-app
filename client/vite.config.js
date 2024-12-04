4ce513c8449a38cebc272c767a730f587cf4677a
Kaustav Das <kaustavdas0228@gmail.com>
2024-12-04 11:48:21 +0530

    update for production

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,8 +4,4 @@ import react from '@vitejs/plugin-react-swc';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  esbuild: {
-    loader: 'jsx', // Tell esbuild to handle .js files as JSX
-    include: /src\/.*\.js$/, // Include JavaScript files in the src folder for JSX parsing
-  },
 });
