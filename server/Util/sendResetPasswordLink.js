a89f348bf478ac6008c51c3d3892a6615f0dbbea
Kaustav Das <kaustavdas0228@gmail.com>
2026-07-04 17:30:23 +0530

    update in nodemailer

--- server/Util/sendResetPasswordLink.js
+++ server/Util/sendResetPasswordLink.js
@@ -7,6 +7,7 @@ const sendResetPasswordLink = async (email,title,link)=> {
     try{
         const transporter = nodemailer.createTransport({
             service: 'gmail',
+            secure: false,
             auth: {
                 user: process.env.EMAIL,
                 pass: process.env.PASSWORDEMAIL,
